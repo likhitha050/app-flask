8a63bfc322f8584011add17775f37de29beb6690
likhitha050 <likhitha1ms24mc050@gmail.com>
2026-01-28 12:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
+@app.route("/")
 def home():
 	return "HELLO THIS IS FROM FLASK WITH CRON TRIGGERS DEPLOYED ON KUBERNETES"
-if__name__=='__main__':
+if__name__=="__main__":
 	app.run(host='0.0.0.0',port=5000)
 
